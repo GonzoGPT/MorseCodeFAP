d815ae891a9f0d2e90cbbede438dbfb47d131714
Иван Морев <iamorev1@mts.ru>
2025-02-03 22:28:27 +0300

    add russian lang

--- morse_code_worker.c
+++ morse_code_worker.c
@@ -2,19 +2,65 @@
 #include <furi_hal.h>
 #include <lib/flipper_format/flipper_format.h>
 
-
 #define TAG "MorseCodeWorker"
 
 #define MORSE_CODE_VERSION 0
 
 //A-Z0-1
-const char morse_array[36][6] ={
-    ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
-    "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-", ".....",
-    "-....", "--...", "---..", "----.", "-----"
-    };
-const char symbol_array[36] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-    'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
+const char morse_array[43][6] = {
+    // Russian alphabet (33 characters)
+    ".-",     // А
+    "-...",   // Б
+    ".--",    // В
+    "--.",    // Г
+    "-..",    // Д
+    ".",      // Е
+    "....-",  // Ж
+    "--..",   // З
+    "..",     // И
+    ".---",   // Й
+    "-.-",    // К
+    ".-..",   // Л
+    "--",     // М
+    "-.",     // Н
+    "---",    // О
+    ".--.",   // П
+    ".-.",    // Р
+    "...",    // С
+    "-",      // Т
+    "..-",    // У
+    "..-.",   // Ф
+    "....",   // Х
+    "-.-.",   // Ц
+    "---.",   // Ч
+    "----",   // Ш
+    "--.-",   // Щ
+    "--.--",  // Ъ
+    "-.--",   // Ы
+    "-..-",   // Ь
+    "..-..",  // Э
+    "..--",   // Ю
+    ".-.-",   // Я
+
+    // Numbers (10 digits)
+    ".----",  // 1
+    "..---",  // 2
+    "...--",  // 3
+    "....-",  // 4
+    ".....",  // 5
+    "-....",  // 6
+    "--...",  // 7
+    "---..",  // 8
+    "----.",  // 9
+    "-----"   // 0
+};
+const unsigned char symbol_array[43][3] = {
+    "А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "Й",
+    "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У",
+    "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э",
+    "Ю", "Я",
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"
+};
 
 struct MorseCodeWorker {
     FuriThread* thread;
@@ -28,30 +74,32 @@ struct MorseCodeWorker {
     FuriString* words;
 };
 
-void morse_code_worker_fill_buffer(MorseCodeWorker* instance, uint32_t duration){
+void morse_code_worker_fill_buffer(MorseCodeWorker* instance, uint32_t duration) {
     FURI_LOG_D("MorseCode: Duration", "%ld", duration);
-    if( duration <= instance->dit_delta)
+    if(duration <= instance->dit_delta)
         furi_string_push_back(instance->buffer, *DOT);
     else if(duration <= (instance->dit_delta * 3))
         furi_string_push_back(instance->buffer, *LINE);
-    if(furi_string_size(instance->buffer) > 5)
+    else
         furi_string_reset(instance->buffer);
+    if(furi_string_size(instance->buffer) > 5) furi_string_reset(instance->buffer);
     FURI_LOG_D("MorseCode: Buffer", "%s", furi_string_get_cstr(instance->buffer));
 }
 
-void morse_code_worker_fill_letter(MorseCodeWorker* instance){
-    if(furi_string_size(instance->words) > 63)
-        furi_string_reset(instance->words);
-    for (size_t i = 0; i < sizeof(morse_array); i++){
-            if(furi_string_cmp_str(instance->buffer, morse_array[i]) == 0){
-                furi_string_push_back(instance->words, symbol_array[i]);
-                furi_string_reset(instance->buffer);
-                break;
-            }        
+void morse_code_worker_fill_letter(MorseCodeWorker* instance) {
+   if(furi_string_size(instance->words) > 63) furi_string_reset(instance->words);
+    for(size_t i = 0; i < sizeof(morse_array)/sizeof(morse_array[0]); i++) {
+        if(furi_string_cmp_str(instance->buffer, morse_array[i]) == 0) {
+            furi_string_push_back(instance->words, symbol_array[i][0]);
+            if(i < 32) { // For Russian characters only
+                furi_string_push_back(instance->words, symbol_array[i][1]);
+            }
+            break;
         }
+    }
+    furi_string_reset(instance->buffer);
     FURI_LOG_D("MorseCode: Words", "%s", furi_string_get_cstr(instance->words));
 }
-
 
 static int32_t morse_code_worker_thread_callback(void* context) {
     furi_assert(context);
@@ -61,40 +109,50 @@ static int32_t morse_code_worker_thread_callback(void* context) {
     uint32_t end_tick = 0;
     bool pushed = true;
     bool spaced = true;
-    while(instance->is_running){
+    while(instance->is_running) {
         furi_delay_ms(SLEEP);
-        if(instance->play){
-            if(!was_playing){
+
+        if(instance->play) {
+            if(!was_playing) {
                 start_tick = furi_get_tick();
-                furi_hal_speaker_start(FREQUENCY, instance->volume);
+                if(furi_hal_speaker_acquire(1000)) {
+                    furi_hal_speaker_start(FREQUENCY, instance->volume);
+                }
                 was_playing = true;
             }
-        }else{
-            if(was_playing){
+        } else {
+            if(was_playing) {
                 pushed = false;
                 spaced = false;
-                furi_hal_speaker_stop();
+                if(furi_hal_speaker_is_mine()) {
+                    furi_hal_speaker_stop();
+                    furi_hal_speaker_release();
+                }
                 end_tick = furi_get_tick();
                 was_playing = false;
                 morse_code_worker_fill_buffer(instance, end_tick - start_tick);
                 start_tick = 0;
             }
         }
-       if(!pushed){
-            if(end_tick + (instance->dit_delta * 3) < furi_get_tick()){
+        if(!pushed) {
+            if(end_tick + (instance->dit_delta * 3) < furi_get_tick()) {
                 //NEW LETTER
-                morse_code_worker_fill_letter(instance);
-                if(instance->callback)
+                if(!furi_string_empty(instance->buffer)) {
+                    morse_code_worker_fill_letter(instance);
+                    if(instance->callback)
                         instance->callback(instance->words, instance->callback_context);
+                } else {
+                    spaced = true;
+                }
                 pushed = true;
             }
         }
-        if(!spaced){
-            if(end_tick + (instance->dit_delta * 7) < furi_get_tick()){
+        if(!spaced) {
+            if(end_tick + (instance->dit_delta * 7) < furi_get_tick()) {
                 //NEW WORD
                 furi_string_push_back(instance->words, *SPACE);
                 if(instance->callback)
-                        instance->callback(instance->words, instance->callback_context);
+                    instance->callback(instance->words, instance->callback_context);
                 spaced = true;
             }
         }
@@ -119,6 +177,8 @@ MorseCodeWorker* morse_code_worker_alloc() {
 
 void morse_code_worker_free(MorseCodeWorker* instance) {
     furi_assert(instance);
+    furi_string_free(instance->buffer);
+    furi_string_free(instance->words);
     furi_thread_free(instance->thread);
     free(instance);
 }
@@ -132,19 +192,25 @@ void morse_code_worker_set_callback(
     instance->callback_context = context;
 }
 
-void morse_code_worker_play(MorseCodeWorker* instance, bool play){
+void morse_code_worker_play(MorseCodeWorker* instance, bool play) {
     furi_assert(instance);
     instance->play = play;
 }
 
-void morse_code_worker_set_volume(MorseCodeWorker* instance, float level){
+void morse_code_worker_set_volume(MorseCodeWorker* instance, float level) {
     furi_assert(instance);
     instance->volume = level;
 }
 
-void morse_code_worker_set_dit_delta(MorseCodeWorker* instance, uint32_t delta){
+void morse_code_worker_set_dit_delta(MorseCodeWorker* instance, uint32_t delta) {
     furi_assert(instance);
     instance->dit_delta = delta;
+}
+
+void morse_code_worker_reset_text(MorseCodeWorker* instance) {
+    furi_assert(instance);
+    furi_string_reset(instance->buffer);
+    furi_string_reset(instance->words);
 }
 
 void morse_code_worker_start(MorseCodeWorker* instance) {
@@ -158,6 +224,7 @@ void morse_code_worker_start(MorseCodeWorker* instance) {
 void morse_code_worker_stop(MorseCodeWorker* instance) {
     furi_assert(instance);
     furi_assert(instance->is_running == true);
+    instance->play = false;
     instance->is_running = false;
     furi_thread_join(instance->thread);
     FURI_LOG_D("MorseCode: Stop", "Stop");
--- morse_code_worker.h
+++ morse_code_worker.h
@@ -4,15 +4,13 @@
 #include <stdint.h>
 #include <furi.h>
 
-#define FREQUENCY 261.63f
+#define FREQUENCY 250.00f
 #define SLEEP 10
 #define DOT "."
 #define LINE "-"
 #define SPACE " "
 
-typedef void (*MorseCodeWorkerCallback)(
-    FuriString* buffer,
-    void* context);
+typedef void (*MorseCodeWorkerCallback)(FuriString* buffer, void* context);
 
 typedef struct MorseCodeWorker MorseCodeWorker;
 
@@ -31,12 +29,8 @@ void morse_code_worker_stop(MorseCodeWorker* instance);
 
 void morse_code_worker_play(MorseCodeWorker* instance, bool play);
 
+void morse_code_worker_reset_text(MorseCodeWorker* instance);
+
 void morse_code_worker_set_volume(MorseCodeWorker* instance, float level);
 
 void morse_code_worker_set_dit_delta(MorseCodeWorker* instance, uint32_t delta);
-
-
-
-
-
-
